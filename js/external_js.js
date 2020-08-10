7c962ec19adc66785e16c4cb823c6cb714e4c5fa
Damien Salazar <salazar.damien.01@gmail.com>
2020-08-10 17:01:14 -0500

    More than halfway throught the exercise

--- js/external_js.js
+++ js/external_js.js
@@ -1,1 +1,26 @@
 "use strict";
+
+
+console.log("Hello from external JavaScript");
+
+var welcome;
+welcome = "Welcome to my Website!"
+alert(welcome)
+
+var question1 = prompt('What is your favorite color ')
+console.log('Great my favorite color is ' + question1 + ' too!' );
+
+var littleMermaidDays = prompt("How Many Days Did You Rent The Little Mermaid?");
+var brotherBearDays = prompt("How Many Days Did You Rent The Brother Bear?");
+var herculesDays = prompt("How Many Days Did You Rent Hercules?");
+var blockBusterBill = (+littleMermaidDays + +brotherBearDays + +herculesDays) * 3;
+alert("Total Blockbuster Bill: $ " + blockBusterBill);
+
+var googleRate = 400;
+var amazonRate = 380;
+var facebookRate = 350;
+var googleHours = prompt("How Many Hours Did You Work For Google This Week?");
+var amazonHours= prompt("How Many Hours Did You Work For Amazon This Week?");
+var facebookHours = prompt("How Many Hours Did You Work For Facebook This Week?");
+var weeksPay = (+googleHours * googleRate) + (+amazonHours * amazonRate) + (+facebookHours * facebookRate);
+console.log("You Made $ " + weeksPay + " This Week.");
