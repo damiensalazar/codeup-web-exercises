dd91c732fc4cc555815389651595609c73278f49
Damien Salazar <salazar.damien.01@gmail.com>
2020-08-10 15:08:10 -0500

    lecture, learning js

---
+++ js/js-lecture.js
@@ -0,0 +1,41 @@
+"use strict";
+//using strict if you type something wrong the console yells at you//
+//scripts don't run the things on your html file only from the js file.//
+
+
+var firstname;
+firstname = "damien"
+console.log(firstname)
+
+var lastname;
+lastname = "salazar";
+console.log(lastname)
+
+console.log(firstname + " " + lastname);
+
+var suprise = "Hello!!!";
+alert(suprise);
+//alert give little popup//
+var giveme;
+giveme = "Hello, Give Me Your Time!";
+alert(giveme);
+
+var question = "Do you like puppies";
+var answer = confirm(question);
+confirm("answer="+answer);
+
+/*var question4 = "Do you want to continue?";
+var answer4 = confirm(question4);
+console.log("answer="+answer4);*/
+
+var question2 = "What is your sign?";
+var answer2 = prompt(question2);
+console.log(answer2);
+
+var question3 = "What is your favorite number?";
+var answer3 = prompt(question3);
+console.log(typeof answer3);
+
+
+
+
