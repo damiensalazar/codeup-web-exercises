fda7712275078da78252ecbba5bd74312d5d2fe5
Damien Salazar <salazar.damien.01@gmail.com>
2020-08-20 11:09:11 -0500

    Finished Arrays Exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,43 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    var names = [
+        'Damien Salazar',
+        'Xavier Salazar',
+        'Ashley Salazar',
+        'Lucas Ryan Salazar'
+    ];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length)
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    for (var i = 0; i < names.length; i++){
+        console.log(names[i])
+    };
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    names.forEach(function(name) {
+        console.log(name);
+    })
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +56,21 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function firstItem(input){
+        return (input[0])
+    }
+    console.log(firstItem(names))
+
+    function secondItem(input){
+        return(input[1])
+
+    }
+    console.log(secondItem(names))
+
+    function lastitem(input){
+        return(input[3])
+    }
+    console.log(lastitem(names))
 
 })();
