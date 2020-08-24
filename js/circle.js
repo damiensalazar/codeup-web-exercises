5ec0dc9b5d8e36cc0db2d3531c1d63e81f826030
Damien Salazar <salazar.damien.01@gmail.com>
2020-08-24 16:58:22 -0500

    Math exercise

--- js/circle.js
+++ js/circle.js
@@ -9,16 +9,23 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return Math.PI * Math.pow(this.radius,2);
+            // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
+            var roundRadius
             // TODO: complete this method.
+            if(doRounding === true){
+                roundRadius = Math.round(this.radius)
+            }else {
+                roundRadius = this.radius
+            }
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
 
@@ -30,6 +37,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
