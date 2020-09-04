a6555e7e9963866a957974a394a682f8d2ae2c47
Damien Salazar <salazar.damien.01@gmail.com>
2020-09-04 09:17:50 -0500

    updated img folder/ morning pushup

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -39,3 +39,35 @@ function average(arr){
         console.log('Enter an array')
     }
 }
+
+function factors(n)
+{
+    var num_factors = [], i;
+
+    for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
+        if (n % i === 0)
+        {
+            num_factors.push(i);
+            if (n / i !== i)
+                num_factors.push(n / i);
+        }
+    num_factors.sort(function(x, y)
+    {
+        return x - y;});  // numeric sort
+    return num_factors;
+}
+ console.log(factors(10));
+console.log(factors(15));
+console.log(factors(30));
+
+function calculate(num) {
+    var str = "0";
+    for (var i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            str += ',' + i;
+        }
+    }
+    console.log(str);
+}
+
+calculate(232);
